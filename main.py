8d0f227e46ec87fedc22348b8c0f7166272966ea
Bartłomiej Koptyra <bartlomiej.koptyra@pwr.edu.pl>
2024-01-30 15:30:16 +0100

    removed whitespace

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.utils import prepare_configuration, seed_everything
 def main():
     seed, base_model_config, lora_config, quantization_config, \
         training_config, data_dict, pad_token_id = prepare_configuration()
-    
+
     seed_everything(seed)
     model = get_model(base_model_config, quantization_config, pad_token_id)
 
